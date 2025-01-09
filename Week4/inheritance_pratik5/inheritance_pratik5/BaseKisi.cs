6b0be87c2f546c4d4c58f07b849c8c9e85154ce0
pelinbasaslan <pelin.basaslan@gmail.com>
2025-01-09 13:00:03 +0300

    inheritance pratik5 eklendi.

---
+++ Week4/inheritance_pratik5/inheritance_pratik5/BaseKisi.cs
@@ -0,0 +1,33 @@
+public class BaseKisi
+{
+    public string Name { get; set; }
+    public string SurName { get; set; }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Kişinin Adı:{Name}, Soyadı:{SurName}.");
+    }
+
+}
+
+public class Ogrenci : BaseKisi
+{
+    public string StudentNumber { get; set; }
+
+    public void DisplayInfoOgrenci()
+    {
+        DisplayInfo();
+        Console.WriteLine($"Öğrenci numarası:{StudentNumber}");
+    }
+}
+
+public class Ogretmen : BaseKisi
+{
+    public double Wage { get; set; }
+
+    public void DisplayInfoOgretmen()
+    {
+        DisplayInfo();
+        Console.WriteLine($"Maaşı: {Wage}");
+    }
+}
---
+++ Week4/inheritance_pratik5/inheritance_pratik5/Program.cs
@@ -0,0 +1,12 @@
+﻿Ogrenci ogrenci = new Ogrenci();
+ogrenci.Name = "Pelin";
+ogrenci.SurName = "Başaslan";
+ogrenci.StudentNumber = "111111";
+
+Ogretmen ogretmen = new Ogretmen();
+ogretmen.Name = "Sertan";
+ogretmen.SurName = "Bozkuş";
+ogretmen.Wage = 100;
+
+ogrenci.DisplayInfoOgrenci();
+ogretmen.DisplayInfoOgretmen();
