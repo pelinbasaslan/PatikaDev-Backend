c47e128642baf0547062004e752053cef8e659a1
pelinbasaslan <pelin.basaslan@gmail.com>
2025-01-09 03:38:37 +0300

    Pratik4 kapsülleme eklendi.

--- Week4/encapsulation-pratik4/encapsulation-pratik4/Araba.cs
+++ Week4/encapsulation-pratik4/encapsulation-pratik4/Araba.cs
@@ -3,9 +3,7 @@ namespace encapsulation_pratik4
     public class Araba
     {
         private int kapiSayisi;
-        public string Model { get; set; }
-        public string Marka { get; set; }
-        public string Renk { get; set; }
+
         public int KapiSayisi
         {
             get
@@ -28,9 +26,32 @@ namespace encapsulation_pratik4
             }
         }
 
+        public string Model { get; set; }
+        public string Marka { get; set; }
+        public string Renk { get; set; }
+
+        public Araba()
+        {
+            KapiSayisi = 4;
+        }
+
+        public Araba(string model, string marka, string renk, int kapiSayisi)
+        {
+            Model = model;
+            Marka = marka;
+            Renk = renk;
+            KapiSayisi = kapiSayisi;
+        }
+
         public void ArabayiTanit()
         {
             Console.WriteLine($"{Model} model arabanın markası {Marka}, rengi {Renk}");
+        }
+
+        public void KapiSayisiniSoyle()
+        {
+            if (kapiSayisi != -1)
+                Console.WriteLine($"Kapı Sayısı: {kapiSayisi}");
         }
 
     }
--- Week4/encapsulation-pratik4/encapsulation-pratik4/Program.cs
+++ Week4/encapsulation-pratik4/encapsulation-pratik4/Program.cs
@@ -2,17 +2,22 @@
 using encapsulation_pratik4;
 
 Araba araba = new Araba();
-araba.Marka = "Mercedes";
-araba.Model = "C200";
-araba.KapiSayisi = 4;
+araba.Marka = "Marka1";
+araba.Model = "Model1";
+araba.KapiSayisi = 3;
 araba.Renk = "kırmızı";
+araba.ArabayiTanit();
+araba.KapiSayisiniSoyle();
 
 Araba araba2 = new Araba();
-araba2.Marka = "BMW";
-araba.Model = "5";
-araba.KapiSayisi = 4;
-araba.Renk = "Siyah";
-
-araba.ArabayiTanit();
+araba2.Marka = "Marka2";
+araba2.Model = "Model2";
+araba2.KapiSayisi = 2;
+araba2.Renk = "siyah";
 araba2.ArabayiTanit();
+araba2.KapiSayisiniSoyle();
+
+Araba araba3 = new Araba("Model3", "Marka3", "bordo", 3);
+araba3.ArabayiTanit();
+araba3.KapiSayisiniSoyle();
 
