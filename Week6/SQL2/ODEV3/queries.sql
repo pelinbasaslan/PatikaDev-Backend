f5d1e569ba4ba522b121a263adb296e09e253734
pelinbasaslan <pelin.basaslan@gmail.com>
2025-01-19 15:04:03 +0300

    SQL oto kaydet format false olarak düzenlendi.

--- Week6/SQL2/ODEV3/queries.sql
+++ Week6/SQL2/ODEV3/queries.sql
@@ -1,27 +1,21 @@
 --1)country tablosunda bulunan country sütunundaki ülke isimlerinden 'A' karakteri ile başlayıp 'a' karakteri ile sonlananları sıralayınız.
 
-SELECT country
-FROM country
+SELECT country FROM country
 WHERE country LIKE 'A%a';
 
 --2)country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karakterden oluşan ve sonu 'n' karakteri ile sonlananları sıralayınız.
  --SELECT country FROM country WHERE LENGTH(country) >= 6 AND country LIKE '%n';
 
-SELECT country
-FROM country
+SELECT country FROM country
 WHERE country LIKE '_____%n';
 
 --3)film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
 --SELECT title FROM film WHERE LOWER(title) LIKE '%t%' GROUP BY title HAVING COUNT(title) >= 4;
 
-SELECT title
-FROM film
+SELECT title FROM film
 WHERE title ILIKE '%t%t%t%t%';
 
 --3)film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
 
-SELECT *
-FROM film
-WHERE title LIKE 'C%'
-    AND length >90
-    AND rental_rate IN (2.99);
+SELECT * FROM film
+WHERE title LIKE 'C%' AND length >90 AND rental_rate IN (2.99);
--- Week6/SQL3/ODEV5/queries.sql
+++ Week6/SQL3/ODEV5/queries.sql
@@ -1,15 +1,13 @@
 --1)film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en uzun (length) 5 filmi sıralayınız.
 
-SELECT title
-FROM film
+SELECT title FROM film
 WHERE title LIKE '%n'
 ORDER BY lENGTH(title) DESC
 LIMIT 5;
 
 --2)film tablosunda bulunan ve film ismi (title) 'n' karakteri ile biten en kısa (length) ikinci(6,7,8,9,10) 5 filmi(6,7,8,9,10) sıralayınız.
 
-SELECT *
-FROM film
+SELECT * FROM film
 WHERE title LIKE '%n'
 ORDER BY length ASC
 OFFSET 5
@@ -17,8 +15,7 @@ LIMIT 5;
 
 --3)customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
 
-SELECT *
-FROM customer
+SELECT * FROM customer
 WHERE store_id=1
 ORDER BY last_name DESC
 LIMIT 4;
