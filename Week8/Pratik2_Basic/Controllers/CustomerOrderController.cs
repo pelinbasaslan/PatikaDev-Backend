2a91707a6143431d91e6a0af9763f09f4299e931
pelinbasaslan <pelin.basaslan@gmail.com>
2025-02-17 20:58:44 +0300

    week8 pratik2_basic is edited.

--- Week8/Pratik2_Basic/Controllers/CustomerOrderController.cs
+++ Week8/Pratik2_Basic/Controllers/CustomerOrderController.cs
@@ -5,23 +5,23 @@ namespace Pratik2_Basic.Controllers
 {
     public class CustomerOrderController : Controller
     {
-        CustomerOrderViewModel newModel = new CustomerOrderViewModel()
+        public IActionResult Index()
         {
-            Customer = new Customer
+            CustomerOrderViewModel newModel = new CustomerOrderViewModel()
             {
-                Id = 1,
-                FirstName = "John",
-                LastName = "Doe",
-                Email = "john@doe.com"
-            },
-            Orders = new List<Order>{
+                Customer = new Customer
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = "john@doe.com"
+                },
+                Orders = new List<Order>{
                 new Order{Id=1, Price=1, ProductName="A", Quantity=1},
                 new Order{Id=2, Price=2, ProductName="B", Quantity=2},
                 new Order{Id=3, Price=3, ProductName="C", Quantity=3}
             }
-        };
-        public IActionResult Index()
-        {
+            };
             return View(newModel);
         }
     }
